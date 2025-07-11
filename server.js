7073b02418dc18bf90bb43f7c67fc01b6a2aa3d2
Vansh6401 <Vanshsharma6401@gmail.com>
2025-07-11 16:12:48 +0530

    update

--- server.js
+++ server.js
@@ -1,29 +1,57 @@
+// import express from "express";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// import userRouter from "./routes/userRoute.js";
+// import cors from "cors"
+// dotenv.config();
+// const app = express();
+// app.use(cors())
+// app.use(express.json());
+// const dbuser = encodeURIComponent(process.env.DBUSER);
+// const dbpass = encodeURIComponent(process.env.DBPASS);
+
+// // mongoose.connect(`mongodb://localhost:27017/cafe`).then(() => {
+// //   app.listen(8080, () => {
+// //     console.log("Server started");
+// //   });
+// // });
+
+// mongoose
+//   .connect(
+//     `mongodb+srv://${dbuser}:${dbpass}@cluster0.qjxhv.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`
+//   )
+//   .then(() => {
+//     app.listen(8080, () => {
+//       console.log("Server started");
+//     });
+//   });
+
+// app.use("/api/users", userRouter);
+
+
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoute.js";
-import cors from "cors"
+import cors from "cors";
+
 dotenv.config();
+
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
+
 const dbuser = encodeURIComponent(process.env.DBUSER);
 const dbpass = encodeURIComponent(process.env.DBPASS);
 
-// mongoose.connect(`mongodb://localhost:27017/cafe`).then(() => {
-//   app.listen(8080, () => {
-//     console.log("Server started");
-//   });
-// });
-
 mongoose
   .connect(
-    `mongodb+srv://${dbuser}:${dbpass}@cluster0.qjxhv.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`
+    `mongodb+srv://${dbuser}:${dbpass}@cluster0.ivvaoj5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
   )
-  .then(() => {
-    app.listen(8080, () => {
-      console.log("Server started");
-    });
-  });
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use("/api/users", userRouter);
+
+// ✅ Export the Express app for Vercel
+export default app;
